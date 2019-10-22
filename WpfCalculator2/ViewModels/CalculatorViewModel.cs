7d55905c7dcea01962f7ea7bc6a84bb788c02694
conway40 <conway40@mail.nmc.edu>
2019-10-22 15:03:54 -0400

    initial commit

---
+++ WpfCalculator2/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfCalculator2.ViewModels
+{
+    class CalculatorViewModel : ObservableObject
+    {
+        public ICommand ButtonCalculateCommand { get; set; }
+
+        private double _result;
+
+        public double Operand1 { get; set; }
+        public double Operand2 { get; set; }
+
+        public double Result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+                OnPropertyChanged("Result");
+            }
+        }
+
+        private void PerformCalculation(object obj)
+        {
+            Result = 55; // value to confirm binding of property
+        }
+
+        public CalculatorViewModel()
+        {
+            Operand1 = 44; //value to confirm binding of property
+            ButtonCalculateCommand = new RelayCommand(new Action<object>(PerformCalculation));
+        }
+    }
+}
